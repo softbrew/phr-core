509f78e196faab6a7983219665aca6ee98ff2049
Milan Karunarathne <mhkarunarathne@gmail.com>
2015-12-03 20:53:57 +0530

    Remove sessions and integrate JWT
    - Remove `Session` and `Cookies`
    - Integrate with new JWT authentication
    - Implemented basic login and token generation

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -5,16 +5,45 @@
 'use strict';
 
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 let LoginRouter = express.Router();
 
-LoginRouter.get('/', (req, res, next) => {
-    res.redirect('/public/index.html');
+// Authenticate user login
+LoginRouter.post('/signin', (req, res) => {
+    //TODO validate username and password
+    console.log('Req Body ', req.body);
+
+    if (!(req.body.username === 'milan' && req.body.password ===
+            'milan123')) {
+        //if is invalid, return 401
+        res.status(401).json({
+            error: 'Wrong username or password.'
+        });
+        return;
+    }
+
+    var profile = {
+        first_name: 'Milan',
+        last_name: 'karunarathne',
+        email: 'mhkarunarathne.com',
+        id: 123
+    };
+
+    // sending the profile inside the token
+    const token = jwt.sign(profile, 'secret', {
+        expiresIn: '10s'
+    });
+
+    res.json({
+        token: token
+    });
 });
 
 LoginRouter.get('/signup', (req, res) => {
-    res.send('viewed this page ' + req.session.views['/signup'] +
-        ' times');
+    res.json({
+        message: 'SignUp successful.'
+    });
 });
 
 export default LoginRouter;
--- src/server/Server.js
+++ src/server/Server.js
@@ -8,31 +8,29 @@
 
 import path from 'path';
 import express from 'express';
-import session from 'express-session';
+import expressJwt from 'express-jwt';
+import bodyParser from 'body-parser';
 
 import LoginRouter from '../routes/Login';
 
-// Policies
-import SessionPolicy from '../config/policy/SessionPolicy';
+import isAdmin from '../config/policy/isAdmin';
 
 let Server = express();
 
-// Setup environment
-//Server.use('/public', express.static('public'));
-Server.use('/public', express.static('/home/milan/softbrew/dashboard/public'));
-Server.use(session({
-    secret: 'session@secret',
-    resave: false,
-    cookie: {
-        maxAge: 60000
-    }
-}));
+/* Setup environment */
+Server.use(bodyParser.json()); // for parsing application/json
+Server.use(bodyParser.urlencoded({
+    extended: true
+})); // for parsing application/x-www-form-urlencoded
 
-// Set Policies
-Server.use(SessionPolicy);
+/* Set Policies */
+Server.all('/admin/*', isAdmin);
+Server.use('/api', expressJwt({
+    secret: 'secret'
+})); // protect /api routes with JWT
 
-// Set routes
-Server.use('/', LoginRouter);
+/* Set routes */
+Server.use('/user', LoginRouter);
 
 
 export default Server;
