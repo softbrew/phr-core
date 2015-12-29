6d7115603f5bf23ff725dcbd8ab83368a2994076
Milan Karunarathne <mhkarunarathne@gmail.com>
2015-12-29 17:59:39 +0530

    Validate login user and Send customize error on Unauthorized requests
    - Validate already logged in user against token
    - Send back customize error message on Unauthorized requests
    - Protect `/user/` routes (exclude `/singin`, `/signup` etc)

--- src/server/Server.js
+++ src/server/Server.js
@@ -28,9 +28,26 @@ Server.all('/admin/*', isAdmin);
 Server.use('/api', expressJwt({
     secret: 'secret'
 })); // protect /api routes with JWT
+Server.use('/user', expressJwt({
+    secret: 'secret'
+}).unless({
+    path: ['/user/signin', '/user/signup', '/user/import/patient']
+})); // protect /user routes with JWT
+
+// Send JSON error when Unauthorized error
+Server.use(function(err, req, res, next) {
+    console.log('Server Error : ', JSON.stringify(err));
+    if(err.name === 'UnauthorizedError') {
+        delete err.inner.stack;
+        res.status(401).json({
+            type: err.code,
+            message: err.message,
+            error: err.inner
+        });
+    }
+});
 
 /* Set routes */
 Server.use('/user', LoginRouter);
-
 
 export default Server;
